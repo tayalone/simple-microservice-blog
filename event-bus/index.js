9b9ca2f8665169544f0e7ba2f2e02864fcb7c580
estellar_boyz <tayalone@gmail.com>
2020-07-17 22:13:16 +0700

    :sparkles: สร้าง pubsub แบบ manual แต่ล่ะ services

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,11 +4,11 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
-app.post('/event', (req, res) => {
-  const { body: event } = req.body
-  axios.post('http://localhost:4000/event', event)
-  axios.post('http://localhost:4001/event', event)
-  axios.post('http://localhost:4002/event', event)
+app.post('/events', (req, res) => {
+  const { body: event } = req
+  axios.post('http://localhost:4000/events', event)
+  axios.post('http://localhost:4001/events', event)
+  axios.post('http://localhost:4002/events', event)
 
   res.send({ status: 'OK' })
 })
